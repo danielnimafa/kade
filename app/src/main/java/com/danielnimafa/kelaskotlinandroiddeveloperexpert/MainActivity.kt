f62e563326b90bb1f3187f499a9575ce7943bd59
danielnimafa <danielnimafa@gmail.com>
2018-12-03 22:21:16 +0700

    jek durung paham aku

--- app/src/main/java/com/danielnimafa/kelaskotlinandroiddeveloperexpert/MainActivity.kt
+++ app/src/main/java/com/danielnimafa/kelaskotlinandroiddeveloperexpert/MainActivity.kt
@@ -4,8 +4,8 @@ import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.text.Editable
 import android.view.View
+import kotlinx.coroutines.*
 import org.jetbrains.anko.*
 import org.jetbrains.anko.design.snackbar
 
@@ -15,6 +15,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         MainUI().setContentView(this)
+
+        coroutineStuff()
+    }
+
+    fun coroutineStuff() {
+
+        val income = GlobalScope.async { amoundOfIncome() }
+        val capital = GlobalScope.async { amoundOfCapital() }
+
+        GlobalScope.launch {
+            println("Your profit is ${income.await() - capital.await()}")
+            delay(3000)
+            cancelCoroutine(income, capital)
+        }
+
+        listenCoroutineCompletion(income, capital)
+    }
+
+    fun listenCoroutineCompletion(income: Deferred<Int>, capital: Deferred<Int>) {
+        if (income.isCompleted && capital.isCompleted) {
+            println("Completed")
+        }
+    }
+
+    fun cancelCoroutine(income: Deferred<Int>, capital: Deferred<Int>) {
+        if (income.isActive && capital.isActive) {
+            income.cancel()
+            capital.cancel()
+        }
+    }
+
+    suspend fun amoundOfCapital(): Int {
+        delay(1000)
+        return 1000000
+    }
+
+    suspend fun amoundOfIncome(): Int {
+        delay(10000)
+        return 1200000
     }
 
     class MainUI : AnkoComponent<MainActivity> {
